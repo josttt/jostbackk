5b46b121c317a9d41143635313cc5972b0d37e09
josttt <josttristan.toomis@tptlive.ee>
2024-05-02 18:57:51 +0300

    shesh

---
+++ seeders/20240417105424-users-seeder.js
@@ -0,0 +1,25 @@
+'use strict';
+const {User} = require('../models/index.js');
+const bcrypt = require('bcrypt');
+const { faker } = require('@faker-js/faker');
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await User.create({
+      name: 'Jost Tristan Toomis',
+      email: 'josttristan.toomis@tptlive.ee',
+      password: bcrypt.hashSync('password', 12)
+    });
+    
+    for(let i = 0; i<10;i++){
+      await User.create({
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        password: bcrypt.hashSync('password', 12)
+      });
+    }
+  },
+    async down (queryInterface, Sequelize) {
+      await queryInterface.bulkDelete('Users', null, {});
+    }
+};
